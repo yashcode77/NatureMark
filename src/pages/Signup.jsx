eca0c01f145473edaa5652adc3f1c3c59de24ec5
Yash <yashchhaproo@gmail.com>
2024-04-17 17:43:11 +0530

    login and signup page

---
+++ src/pages/Signup.jsx
@@ -0,0 +1,136 @@
+import React from 'react';
+import { Link } from "react-router-dom";
+
+const Signup = () => {
+  return (
+    <div className='flex h-screen justify-center items-center bg-neutral-100'>
+      <div className='bg-white flex flex-row-reverse border-2 h-3/5 w-1/2 rounded-2xl shadow-2xl'>
+        <div className='flex-grow border-r-2 w-1/2'>
+        <div className="min-h-full flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">Create your account</h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              
+              
+            </p>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" defaultValue="true" />
+            <div className="rounded-md shadow-sm -space-y-px">
+              <div className=''>
+                <label htmlFor="name" className="sr-only">
+                  Name
+                </label>
+                <input
+                  id="name"
+                  name="name"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block
+                  w-full px-3 py-2 border border-gray-300
+                  placeholder-gray-500 text-gray-900 rounded-t-md
+                  focus:outline-none focus:ring-green-500
+                  focus:border-green-500 focus:z-10 sm:text-sm"
+                  placeholder="Name"
+                />
+              </div>
+              <div>
+                <label htmlFor="email-address" className="sr-only">
+                  Email address
+                </label>
+                <input
+                  id="email-address"
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  required
+                  className="appearance-none rounded-none relative block
+                  w-full px-3 py-2 border border-gray-300
+                  placeholder-gray-500 text-gray-900
+                  focus:outline-none focus:ring-green-500
+                  focus:border-green-500 focus:z-10 sm:text-sm"
+                  placeholder="Email address"
+                />
+              </div>
+              <div>
+                <label htmlFor="password" className="sr-only">
+                  Password
+                </label>
+                <input
+                  id="password"
+                  name="password"
+                  type="password"
+                  autoComplete="current-password"
+                  required
+                  className="appearance-none rounded-none relative block
+                  w-full px-3 py-2 border border-gray-300
+                  placeholder-gray-500 text-gray-900 rounded-b-md
+                  focus:outline-none focus:ring-green-500
+                  focus:border-green-500 focus:z-10 sm:text-sm"
+                  placeholder="Password"
+                />
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input
+                  id="remember-me"
+                  name="remember-me"
+                  type="checkbox"
+                  className="h-4 w-4 text-green-600 focus:ring-green-500
+                  border-gray-300 rounded"
+                />
+                <label htmlFor="remember-me" className="ml-2 block text-sm text-gray-900">
+                  I agree to the terms and conditions
+                </label>
+              </div>
+
+              <div className="text-sm">
+                
+              </div>
+            </div>
+
+            <div>
+              <button
+                type="submit"
+                className="group relative w-full flex justify-center
+                py-2 px-4 border border-transparent text-sm font-medium
+                rounded-md text-white bg-green-600 hover:bg-green-700
+                focus:outline-none focus:ring-2 focus:ring-offset-2
+                focus:ring-green-500"
+              >
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  {/* <LockClosedIcon className="h-5 w-5 text-green-500 group-hover:text-green-400"
+                  aria-hidden="true" /> */}
+                </span>
+                Sign up
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+        </div>
+        <div className='w-1/2 bg-gradient-to-br from-green-900 to-green-400 text-white flex-col gap-4 justify-center items-center rounded-tl-2xl rounded-bl-2xl content-center'>
+          <div className='h-3/4 rounded-2xl flex flex-col gap-6 p-7 justify-center'>
+            <div className='flex justify-center text-3xl font-extrabold text-center'>Welcome to NatureMark Systems</div>
+            <div className='flex-col justify-center text-center'>Already have an account?</div>
+            <div className='flex justify-center'>
+              <button class="bg-black hover:bg-black-700 text-white font-bold py-2 px-4 rounded-full w-1/2 flex justify-center content-center">
+                <Link to="/">
+                Sign in
+                </Link>
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+
+export default Signup;
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+const { colors: defaultColors } = require('tailwindcss/defaultTheme')
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
