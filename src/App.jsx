dee63320d493207a0ca722daf54b2270b4392b43
tanmayGypt <tanmay631@gmail.com>
2024-04-19 21:05:39 +0530

    Contact Page introduced, Not responsive

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,21 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Login from './pages/Login';
-import Signup from './pages/Signup';
-import Footer from './components/Footer';
-import Navbar from './components/Navbar';
-
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Footer from "./components/Footer";
+import Navbar from "./components/Navbar";
+import Contact from "./components/ContactUs/Contact";
 
 function App() {
   return (
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path='/' element={<Login />} />
-        <Route path='/signup' element={<Signup />} />
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="/Contact" element={<Contact />} />
       </Routes>
-      <Footer />
     </BrowserRouter>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -3,7 +3,7 @@ import Form from "../Login_signUp/Form";
 import SideOption from "../Login_signUp/SideOption";
 function Login() {
   return (
-    <div className="flex justify-center items-center min-h-screen h-screen bg-neutral-100">
+    <div className="flex justify-center items-center min-h-screen h-screen bg-neutral-100 w-screen">
       <div className="bg-white flex flex-col sm:flex-row border-2 h-3/5 w-screen sm:max-w-screen-md rounded-2xl shadow-2xl">
         <Form />
         <SideOption />
